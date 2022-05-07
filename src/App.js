528c507b317022689b7ba9d9b5866c4ba85c54ef
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-05-07 13:46:01 +0900

    Section3 37-43 state, useState, passing data from child to parent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expense from "./components/Expenses/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -23,9 +24,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expenseData) => {
+    console.log("App.addExpenseHandler() ::: ", expenseData);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expense expenses={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    // console.log("NewExpense.saveExpenseDataHandler() ::: ", expenseData);
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+}
+
+export default NewExpense;
