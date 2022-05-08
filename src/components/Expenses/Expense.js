0752234c4396f94a506e4c747b3114718b91b008
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-05-08 21:01:42 +0900

    Section3 ExpenseFilter

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import Card from "../UI/Card";
 import "./Expense.css";
 
 const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2019");
+  const changeFilterHandler = (year) => {
+    console.log(`Expense.changeFilterHandler() ::: ${year}`);
+    setFilteredYear(year);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        date={props.expenses[0].date}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={changeFilterHandler}
+        />
+        <ExpenseItem
+          date={props.expenses[0].date}
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
 };
 
---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,29 @@
+import "./ExpenseFilter.css";
+
+function ExpenseFilter(props) {
+  const filterChangeHandler = (event) => {
+    console.log(event.target.value);
+    props.onChangeFilter(event.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label htmlFor="filter_year">Filter by year</label>
+        <select
+          name="filter_year"
+          id="filter_year"
+          value={props.selected}
+          onChange={filterChangeHandler}
+        >
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseFilter;
