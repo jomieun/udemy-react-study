ea50ce70ffa61dc4ebd06e65ba0ab0ff9db20245
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-05-16 07:33:54 +0900

    Section5 conditional contents 과제

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,42 @@
+import { useState } from "react";
+
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    disableFormHandler();
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  const enableFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const disableFormHandler = () => {
+    setShowForm(false);
+  };
+
+  let formContent = (
+    <button onClick={enableFormHandler}>Add New Expense</button>
   );
+
+  if (showForm) {
+    formContent = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={disableFormHandler}
+      />
+    );
+  }
+
+  return <div className="new-expense">{formContent}</div>;
 }
 
 export default NewExpense;
