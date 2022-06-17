a53ab2093432c5fb9971a224d4afaa5986cb95f7
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-06-17 22:14:53 +0900

    Section 12 자식 컴포넌트 재평가

--- 12-starting-setup/src/App.js
+++ 12-starting-setup/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 import "./App.css";
 
 function App() {
+  console.log("App Running!");
   const [showParagraph, setShowParagraph] = useState(false);
 
   const toggleParagraphHandler = () => {
@@ -12,7 +14,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
