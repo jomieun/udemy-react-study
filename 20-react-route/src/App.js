8a46d1eece52bb42f3bd171a644dc4e157c1ee15
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-07-27 23:54:40 +0900

    Section 20 Route, NavLink, Link, Redirect, Switch

---
+++ 20-react-route/src/App.js
@@ -0,0 +1,32 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import MainHeader from "./components/MainHeader";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ 20-react-route/src/pages/ProductDetails.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+  const params = useParams();
+
+  console.log(params.productId);
+  return (
+    <section>
+      <h1>Product Details</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
+};
+
+export default ProductDetails;
