af19ac9ecec41404b2718de3307ed83b1c1146c0
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-08-01 23:17:35 +0900

    Section 20 중첩경로, 예외처리, 404 핸들링, useHistory, prompt

---
+++ 20-route-practice/src/pages/QuoteDetail.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+
+import Commnets from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Maxim",
+    text: "Learning React is great!",
+  },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Commnets />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
