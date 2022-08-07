44d9968c5d6bea4959a088e31c17fca89be2a1d4
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-08-07 13:45:40 +0900

    섹션 22 change password

--- 21-auth-token/src/components/Auth/AuthForm.js
+++ 21-auth-token/src/components/Auth/AuthForm.js
@@ -1,9 +1,11 @@
 import { useContext, useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -49,6 +51,7 @@ const AuthForm = () => {
         return res.json().then((data) => {
           // console.log(data);
           authCtx.login(data.idToken);
+          history.replace("/");
         });
       } else {
         return res
