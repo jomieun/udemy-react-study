49ea6c4bef5cd84130e055c8635d83807204af55
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-10-02 23:55:49 +0900

    Section 25 380 context API로 변경

--- 25-replace-redux/src/containers/Products.js
+++ 25-replace-redux/src/containers/Products.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
+import { ProductContext } from "../context/product-context";
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+const Products = (props) => {
+  const productList = useContext(ProductContext).products;
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
