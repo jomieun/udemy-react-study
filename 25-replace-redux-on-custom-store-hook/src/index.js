ebc765a94c5642b1735583a360ca38d306086762
DESKTOP-6O5Q33Q\Lee <jomieuns2@gmail.com>
2022-10-05 00:14:07 +0900

    Section 25 스토어로 구현

--- 25-replace-redux-on-custom-store-hook/src/index.js
+++ 25-replace-redux-on-custom-store-hook/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import App from "./App";
+import "./index.css";
 
-import './index.css';
-import App from './App';
-import ProductsProvider from './context/products-context';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import configureStore from "./hooks-store/products-store";
+
+configureStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
